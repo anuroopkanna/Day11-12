ecae3825b5be28727a964561844d3686fbc91d4c
anuroopkanna <anuroopindia16@gmail.com>
2023-03-31 15:53:44 +0530

    Day11&12

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.BridgeLabz1112;
+
+public class Main {
+    public static void main(String[] args) {
+        StockManagement stockManagement = new StockManagement();
+        stockManagement.createStock();
+        stockManagement.display();
+        stockManagement.totalvalueofallstocks();
+    }
+}
